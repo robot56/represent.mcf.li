ec143749a033cb0211ab10598254e0c972c9dcb6
Samuel Ryan <sam@samryan.co.uk>
2012-08-17 07:29:06 +0100

    analytics

--- views/username.php
+++ views/username.php
@@ -5,8 +5,21 @@
     <title>Username update &mdash; represent.mcf.li</title>
     <link rel="author" href="humans.txt" />
     <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.min.css" type="text/css" />
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-29439846-15']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+    </script>
 </head>
 <body>
     <div class="container" id="side">
